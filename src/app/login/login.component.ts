76be03a0e93b45fd5858aeb92856e61f61f3e5d2
foxday <teruelpablo@gmail.com>
2021-07-01 19:18:36 -0300

    Primera implementación de login contra el back.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthServiceService } from '../service/auth-service.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  formGroup!: FormGroup;
 
-  constructor(protected router : Router) { }
+  constructor(
+    protected router: Router,
+    private authService: AuthServiceService
+  ) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
-  Login(){
+  initForm() {
+    this.formGroup = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
 
-    this.router.navigate(["/"]);
+  Login() {
+    // this.router.navigate(['/']);
+    if (this.formGroup.valid) {
+      this.authService.login(this.formGroup.value).subscribe((result) => {
+        if (result.success) {
+          console.log(result);
+          alert(result.message);
+        } else {
+          alert(result.message);
+        }
+      });
+    }
   }
 }
