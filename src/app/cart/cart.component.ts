18213422c1501d5c5c255722e42d93c4d83a4fc3
foxday <teruelpablo@gmail.com>
2021-07-02 23:13:27 -0300

    Redirecciona de cart si no está logueado.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  constructor(protected router: Router) {}
 
   ngOnInit(): void {
+    if (!localStorage.getItem('loggedUser')) {
+      alert('Necesita estar logueado para acceder al carrito');
+      this.router.navigate(['/login']);
+    }
   }
-
 }
